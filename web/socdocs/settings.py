979924c8b313dc2f15ddd2447d8f5577d111dd0c
profz <jlzeller@fhsu.edu>
2025-11-03 22:52:18 -0600

    Add "accounts" app with authentication views, templates, and configurations

--- web/socdocs/settings.py
+++ web/socdocs/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "policies",
     "diagrams",
     "grading",
+    "accounts",
 ]
 
 MIDDLEWARE = [
--- web/socdocs/urls.py
+++ web/socdocs/urls.py
@@ -1,19 +1,17 @@
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
+from accounts.views import SignUpView  # we'll add this view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/login/",  auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
+    path("accounts/signup/", SignUpView.as_view(), name="signup"),
+
     path("markdownx/", include("markdownx.urls")),
     path("docs/", include("docs.urls")),
     path("policies/", include("policies.urls")),
     path("diagrams/", include("diagrams.urls")),
     path("grading/", include("grading.urls")),
-
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
